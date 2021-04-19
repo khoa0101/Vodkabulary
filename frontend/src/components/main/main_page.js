6c2479680a85f789a73342f1f6eebf99c0517532
Kevin <kevinsebastianmathews@gmail.com>
2021-04-19 13:32:54 -0700

    user auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Vodkabulary</h1>
+        <footer>
+          Built by Chris, Kevin, Cristiano, and Khoa
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
