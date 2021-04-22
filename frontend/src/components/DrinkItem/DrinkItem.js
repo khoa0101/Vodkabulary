d6b83feb2f973af2e0110bb8c5be6dbc8ebb83b7
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-21 22:38:40 -0700

    complete form

--- frontend/src/components/DrinkItem/DrinkItem.js
+++ frontend/src/components/DrinkItem/DrinkItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import drink from '../../../../validation/drink';
 
 const drinkItem = (props) => (
     <div className='DrinkItem'>
--- frontend/src/components/Drinks/drink_item_container.js
+++ frontend/src/components/Drinks/drink_item_container.js
@@ -4,16 +4,13 @@ import { createDrink } from "../../actions/drink_actions";
 import { fetchDrink } from "../../actions/drink_actions"
 
 const mapStateToProps = (state, ownProps) => ({   
-        currentUser: state.session.id,
+        currentUser: state.session.user.id,
         drink: state.entities.drinks[ownProps.match.params.id]
-        
-
-        
     })
 
 const mapDispatchToProps = dispatch => ({
     action: drink => dispatch(createDrink(drink)),
-    createDrink: drink => dispatch(createDrink(drink)),
+    processForm: drink => dispatch(createDrink(drink)),
     fetchDrink: id => dispatch(fetchDrink(id)) 
 
 });
