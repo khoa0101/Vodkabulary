7d24b5f2463ebde521c711ffd5cea1f2bcf5bee3
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-22 20:01:23 -0700

    add error page

---
+++ frontend/src/components/error/error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <div >
+        <h1>This page is no longer available</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
