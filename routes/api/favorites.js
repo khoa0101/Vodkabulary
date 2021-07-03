d0b168c927d13933e42b9311e7d76f1405af66a4
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-07-03 07:16:40 -0700

    Add method to call favorite user posts

--- routes/api/favorites.js
+++ routes/api/favorites.js
@@ -4,40 +4,44 @@ const passport = require("passport");
 const Drink = require("../../models/Drink");
 const User = require("../../models/User");
 
-
-router.post('/', 
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-  const { drinkId, userId } = req.body;
-
-  Drink.find({_id: drinkId})
-  .then(drink => {
-    drink = drink[0];
-    drink.favorites.push(userId);
-    drink.save().then(() => {
-      Drink.find({_id: drinkId})
-      .populate("user", "username")
-      .populate("favorites", 'username')
-      .then(drink => {
-        res.json(drink)
-      })
-    })
-  })
+// Posts.find({ likes: userId }).populate("likes");
+router.get("/:userId", (req, res) => {
+  Drink.find({ favorites: req.params.userId }).then((drink) => res.json(drink));
 });
 
-router.delete('/:drinkId', (req, res) => {
-  Drink.findById(req.params.drinkId)
-    .then(drink => {
-      drink.favorites.pull(req.body.userId);
-      return drink.save().then(() => {
-        Drink.find({_id: req.params.drinkId})
-        .populate("user", "username")
-        .populate("favorites", 'username')
-        .then(drink => {
-          res.json(drink)
-        })
-      })
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { drinkId, userId } = req.body;
+
+    Drink.find({ _id: drinkId }).then((drink) => {
+      drink = drink[0];
+      drink.favorites.push(userId);
+      drink.save().then(() => {
+        Drink.find({ _id: drinkId })
+          .populate("user", "username")
+          .populate("favorites", "username")
+          .then((drink) => {
+            res.json(drink);
+          });
+      });
     });
+  }
+);
+
+router.delete("/:drinkId", (req, res) => {
+  Drink.findById(req.params.drinkId).then((drink) => {
+    drink.favorites.pull(req.body.userId);
+    return drink.save().then(() => {
+      Drink.find({ _id: req.params.drinkId })
+        .populate("user", "username")
+        .populate("favorites", "username")
+        .then((drink) => {
+          res.json(drink);
+        });
+    });
+  });
 });
 
 module.exports = router;
